73b5a737b08fd3e33327b26da521065eb37e42b3
simonszabo <simimi129@gmail.com>
2022-10-26 20:57:15 +0200

    init

--- Project1/ContentView.swift
+++ Project1/ContentView.swift
@@ -1,26 +1,53 @@
-//
-//  ContentView.swift
-//  Project1
-//
-//  Created by Simon Szabo on 2022. 10. 26..
-//
-
 import SwiftUI
+import Combine
+
+class DataSource: ObservableObject {
+  let didChange = PassthroughSubject<Void, Never>()
+  var pictures = [String]()
+  
+  init() {
+    let fm = FileManager.default
+    
+    if let path = Bundle.main.resourcePath,
+       let items = try? fm.contentsOfDirectory(atPath: path) {
+      for item in items {
+        if item.hasPrefix("nssl") {
+          pictures.append(item)
+        }
+      }
+    }
+    didChange.send(())
+  }
+}
+
+struct DetailView: View {
+  var selectedImage: String
+  
+  var body: some View {
+    let img = UIImage(named: selectedImage)!
+    return Image(uiImage: img)
+      .resizable()
+      .aspectRatio(contentMode: .fit)
+      .navigationBarTitle(Text(selectedImage), displayMode: .inline)
+  }
+}
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @ObservedObject var dataSource = DataSource()
+  
+  var body: some View {
+    NavigationView {
+      List(dataSource.pictures, id: \.self) { picture in
+        NavigationLink(destination: DetailView(selectedImage: picture)){
+          Text(picture)
         }
-        .padding()
+      }.navigationBarTitle(Text("Storm Viewer"))
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
